fb9b73ec624ca69a51f0974a7f850600057e649a
Nargiz-Aliyeva <nargizaliyeva0120@gmail.com>
2026-02-11 16:16:57 +0400

    Add all tasks

---
+++ d1/src/Grammar.java
@@ -0,0 +1,25 @@
+public class Grammar {
+    // object oriented programming 1 1ine problem yaratmayan kodlar yazmaq ve 1 yerde yaz defelerle istifade et
+    //class
+    // acces modifire
+    // class adlari böyüklə yazılır eger 1 adda birden cox soz varsa her bir söz böyük hərflə yazılıtr MyFirstClass
+    // Metod - Metod yalniz class daxilinde yazila biler
+    //Javada metod adı camelCase yazılır yeni  ilk söz ilk herf balaca
+    //camelCase - metod adinda
+    // PascalCase - Class adinda
+    // snake_case -
+    // data type -
+    // 1primitive data type  - kicik herfle yazilir - int , float, double , char , boolean
+    // 2 none primitive data type (wrapper) - boyukle yazilir  - class , String
+   // comment
+    // Operatorlar - int i =2
+     // i+=6
+    // i = i+6 = 8
+    // i-=2 = i = i-2
+
+    // i++ = i = i+1
+
+
+
+
+}
---
+++ d1/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       // System.out.println("Hello world");
+    //string username = "jet001";
+        //int a = 5;
+        //int b = 6;
+        /*int c = a+b;
+        boolean d = c<0;
+        System.out.println(c);
+        System.out.println(d);
+        boolean y = true;
+        System.out.println(y);
+        Scanner scanner = new Scanner(System.in); // input
+        System.out.print("Enter first number :");
+        int e  = scanner.nextInt();
+        System.out.print("Enter second number : ");
+        int z = scanner.nextInt();
+        System.out.print("Result: ");
+        System.out.println(z*e); // output
+        // practise et
+*/
+
+     /*   String username = "agent 007";
+      //  System.out.print("Welcome, ");
+        //System.out.println(username);
+        System.out.println("Welcome, "+ username+(3+5));
+       */
+        /*
+         int i = 8 ;
+         i+=4;
+         i-=2;
+         i++;
+         i*=5;
+         i/=2;
+        System.out.println(i);
+*/
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter your name please: ");
+String name = scanner.next();
+System.out.print("Enter your age please: ");
+int age = scanner.nextInt();
+System.out.print("Your result infprmation: " +name + " "+ (age>18));
+
+
+    }
+}
